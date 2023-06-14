5b4776f0d12e4e317c11b4f6144c046dd7556b9c
Xar0P <pietroricardocres@proton.me>
2023-06-14 15:20:43 -0300

    create excel file if not exists and get path by .env

--- tests/tracking/tracking.spec.ts
+++ tests/tracking/tracking.spec.ts
@@ -3,8 +3,11 @@ import tracknet from "./tracknet.spec";
 import ship24 from "./ship24.spec";
 import mercadoLivre from "./mercado-livre.spec";
 import * as fs from "fs/promises";
+import { existsSync } from "fs";
 import aliExpress from "./aliexpress.spec";
 import Excel from 'exceljs'
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export interface Products {
   title: string;
@@ -14,19 +17,35 @@ export interface Products {
 }
 
 function saveAsXSLX(content) {
-  const googleDriveUrl = 'G:\\.shortcut-targets-by-id\\1-lkork0_DFuhDI-Ened3syey2-51Qeha\\ETCETERA GERAL\\Rastreamentos\\Rastreios.xlsx'
-
+  const DIR_SAVE = process.env.DIR_SAVE?.replace(/\\/g, '\\\\') as string
   const workbook = new Excel.Workbook();
 
-  workbook.xlsx.readFile(googleDriveUrl).then(() => {
-    const sheet = workbook.getWorksheet('Rastreamento')
+  if (existsSync(DIR_SAVE)) {
+    workbook.xlsx.readFile(DIR_SAVE).then(() => {
+      const sheet = workbook.getWorksheet('Plan1')
+
+      for (const track of content) {
+        const row = sheet.addRow([track.title, track.code, track.time || "Não encontrado", track.message || 'Não encontrado', new Date().toLocaleString()])
+        row.commit();
+      }
+      return workbook.xlsx.writeFile(DIR_SAVE);
+    })
+  } else {
+    const sheet = workbook.addWorksheet('Plan1');
+
+    sheet.getCell('A1').value = 'Nome';
+    sheet.getCell('B1').value = 'Código';
+    sheet.getCell('C1').value = 'Data do último status';
+    sheet.getCell('D1').value = 'Último status';
+    sheet.getCell('E1').value = 'Data do rastreamento';
 
     for (const track of content) {
       const row = sheet.addRow([track.title, track.code, track.time || "Não encontrado", track.message || 'Não encontrado', new Date().toLocaleString()])
       row.commit();
     }
-    return workbook.xlsx.writeFile(googleDriveUrl);
-  })
+
+    workbook.xlsx.writeFile(DIR_SAVE);
+  }
 }
 
 function handleTrackings(trackings) {
